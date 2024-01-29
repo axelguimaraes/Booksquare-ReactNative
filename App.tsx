714e039baaec3ac5ab6caab3d3d78d5e70511c30
axelguimaraes <axelguimaraes2@gmail.com>
2024-01-29 16:40:01 +0000

    added example components

---
+++ App.tsx
@@ -0,0 +1,8 @@
+import "react-native-gesture-handler";
+import React from "react";
+import AppNavigation from "./Navigation";
+function App() {
+  return <AppNavigation />;
+}
+
+export default App;
