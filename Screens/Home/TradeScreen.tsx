962822884268c83f5d72d2ea1b500f1df09785cf
axelguimaraes <axelguimaraes2@gmail.com>
2024-02-05 16:09:16 +0000

    Added trade screen

--- Screens/Home/TradeScreen.tsx
+++ Screens/Home/TradeScreen.tsx
@@ -1,10 +1,35 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, FlatList } from 'react-native';
+import BookCard from '../../Components/BookCard';
+import { getAllBooks } from '../../Services/BooksService';
+import { Book, TransactionType } from '../../Models/Book';
 
-const TradeScreen = () => (
-  <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-    <Text>Third Tab Content</Text>
-  </View>
-);
+const TradeScreen: React.FC = () => {
+  const [books, setBooks] = useState<Book[]>([]);
+
+  useEffect(() => {
+    // Fetch books data when the component mounts
+    const fetchBooks = async () => {
+      try {
+        const fetchedBooks: Book[] = await getAllBooks(TransactionType.TRADE); 
+        setBooks(fetchedBooks); 
+      } catch (error) {
+        console.error('Error fetching books:', error);
+      }
+    };
+
+    fetchBooks();
+  }, []);
+
+  return (
+    <View style={{ flex: 1 }}>
+      <FlatList
+        data={books}
+        renderItem={({ item }) => <BookCard book={item} />}
+        keyExtractor={(item) => item.id.toString()}
+      />
+    </View>
+  );
+};
 
 export default TradeScreen;
